6ceb5b32158916008e62c764d21e619536f67686
Julie Soohoo <jsx1219@berkeley.edu>
2017-11-03 03:27:27 +0000

    [feat] clean up step definitions and unneeded comments

--- features/step_definitions/diff_users_steps.rb
+++ features/step_definitions/diff_users_steps.rb
@@ -1,21 +1,11 @@
 When /^I fill in the fields as follows:$/ do |table|
-#  print(page.body)
  table.hashes.each do |t|
-   case t[:value]
-#   when /^select date "(.*)"$/
-#      steps %Q{When I select "#{$1}" as the "#{t[:field]}" date}
-   when /^selected (.*)"$/
-     steps %Q{When I select "#{$1}" from "#{t[:field]}"}
-#   when /^(un)?checked$/
-#      steps %Q{When I #{$1}check "#{t[:field]}"}
-   else
-     if "#{t[:field]}" == "Type of user"
-       select "#{t[:value]}" , :from => "user[type_user]"
-     else
-       steps %Q{When I fill in "#{t[:field]}" with "#{t[:value]}"}
-     end
-   end
- end
+    if "#{t[:field]}" == "Type of user"
+      select "#{t[:value]}" , :from => "user[type_user]"
+    else
+      steps %Q{When I fill in "#{t[:field]}" with "#{t[:value]}"}
+    end
+  end
 end
 
 Then /^I should see "(.*)" has type "(.*)"$/ do |username, type|
