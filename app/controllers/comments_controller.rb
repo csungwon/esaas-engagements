ccdcf0205d367ea2e0b0a1ea2650967907324bf6
Sungwon Cho <csungwon@berkeley.edu>
2017-11-14 13:54:18 -0800

    [refactor] refactor comments_controller and apps/show.html.haml
    refactored comments_controller methods to DRY some common steps:
      - setting @comment with some comment id
      - authenticate user to edit and destroy a comment
    
    refactored apps/show.html.haml variables and form fields
      - instead of session[:user_id] used @current_user
      - added hidden fields for :app_id and :user_id
      - simplified param list for routing paths

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,44 +1,48 @@
 class CommentsController < ApplicationController
+	before_action :logged_in?
+	before_action :set_comment, only: [:edit, :destroy, :update]
+	before_action :authenticate, only: [:edit, :destroy, :update]
+
 	def new
 		@comment = Comment.new
 	end
 
 	def create
-		if params["comment"]["content"] =~ /^\s*$/
-			flash[:error] = "Comment cannot be empty!"
-			redirect_to app_path(params[:app_id]) and return
+		@comment = Comment.create(comment_params)
+		unless @comment.update(comment_params)
+			@errors = @comment.errors.full_messages
 		end
-		@comment = App.find(params[:app_id]).comments.create!(comment_params)
-		@comment.update_attributes(:username => User.find(session[:user_id]).name, :user_id => session[:user_id])
-		redirect_to app_path(@comment.app_id)
+		redirect_to app_path(params[:app_id])
 	end
 
 	def edit
-		@comment = Comment.find(params[:id])
-		if @comment.user_id != session[:user_id]
-			flash[:error] = "You don't have authorization to edit #{@comment.username}'s comment!"
-			redirect_to app_path(@comment.app_id)
-		end
 	end
 
 	def destroy
-		@comment = Comment.find(params[:id])
-		if @comment.user_id != session[:user_id]
-			flash[:error] = "You don't have authorization to delete #{@comment.username}'s comment!"
-			redirect_to app_path(@comment.app_id)
-		else
-			@comment.destroy
-			redirect_to app_path(@comment.app_id)
-		end
+		@comment.destroy
+		redirect_to app_path(@comment.app)
 	end
 
 	def update
-		@comment = Comment.find params[:id]
-		@comment.update_attributes!(comment_params)
+		unless @comment.update(comment_params)
+			@errors = @comment.erros.full_messages
+		end
 		redirect_to app_path(@comment.app_id)
 	end
 
+private
 	def comment_params
-		params.require(:comment).permit(:username, :user_id, :content, :app_id)
+		params.require(:comment).permit(:content, :app_id, :user_id)
+	end
+
+	def authenticate
+		unless @comment.user_id == session[:user_id]
+			flash[:alert] = "You don't have authorization to #{@comment.user.name}'s comment!"
+			redirect_to app_path(@comment.app)
+		end
+	end
+
+	def set_comment
+		@comment = Comment.find params[:id]
 	end
 end
