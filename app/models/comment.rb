e929d97fbb6a05329ffcaecd2c5389f4d61f033a
Sungwon Cho <csungwon@berkeley.edu>
2017-11-14 10:35:55 -0800

    [feat] refine Comment model to be properly associated with User and App
    Previously Comment was only associated with through app_id or user_id;
    there was no direct association between models. Comment now associates to
    Apps and Users as :has_many relationship. Removed unnecessary columns from
    Comment: :username, :when. Also added :comment_type attribute to Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ActiveRecord::Base
 	belongs_to :app
+	belongs_to :user
+
+	validates_presence_of :content, :app_id, :user_id
+
+	default_scope { order(:created_at => :asc) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104080350) do
+ActiveRecord::Schema.define(version: 20171114183229) do
 
   create_table "apps", force: :cascade do |t|
     t.integer  "org_id"
@@ -28,13 +28,12 @@ ActiveRecord::Schema.define(version: 20171104080350) do
   add_index "apps", ["org_id"], name: "index_apps_on_org_id"
 
   create_table "comments", force: :cascade do |t|
-    t.string   "username"
     t.string   "content"
-    t.datetime "when"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.integer  "app_id"
     t.integer  "user_id"
+    t.integer  "comment_type"
   end
 
   add_index "comments", ["app_id"], name: "index_comments_on_app_id"
