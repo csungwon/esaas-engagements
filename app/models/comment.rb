14bbc6bf38168341378de083d4e2c194de9752db
Sungwon Cho <csungwon@berkeley.edu>
2017-11-14 20:19:11 -0800

    [feat] enable filtering by comment type
    add options for `comment_type` as a `enum`. Three possible types:
    `contact_status`, `app_functionality`, and `general`
    add javascript code to toggle certain types of comments
    add styles(colors) to each type of comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,6 @@ class Comment < ActiveRecord::Base
 	validates_presence_of :content, :app_id, :user_id
 
 	default_scope { order(:created_at => :asc) }
+
+	enum :comment_type => { :contact_status => 0, :app_functionality => 1, :general => 2}
 end
