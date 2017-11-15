750e9e936690329c5ff7166425b32df2c27e6efe
Sungwon Cho <csungwon@berkeley.edu>
2017-11-15 14:49:53 -0800

    [feat] Org can have many comments, and it has its own deatils page
    Polymorpic association is set up between comments and App/Org; now
    comments can belong to any of `Commentable` class. Appropriate
    controllers, routing, views were either created or modified. The
    Org now has a details page (Org#show) for each org object. Like
    the app page, it contains description, comment section, and app
    list that belong to that org. Contact information section is also
    added to the org#show page.
    
    Developer Note:
    	- migration is needed for comment model for polymorphic association
    	- removed comment:string attr from org model.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,39 +3,34 @@ class CommentsController < ApplicationController
 	before_action :set_comment, only: [:edit, :destroy, :update]
 	before_action :authenticate, only: [:edit, :destroy, :update]
 
-	def new
-		@comment = Comment.new
-	end
-
 	def create
-		@comment = Comment.create(comment_params)
-		redirect_to app_path(@comment.app)
-	end
-
-	def edit
+		@comment = @commentable.comments.new(comment_params)
+		@comment.user = @current_user
+		@comment.save
+		redirect_to @commentable
 	end
 
 	def destroy
 		@comment.destroy
-		redirect_to app_path(@comment.app)
+		redirect_to @comment.commentable
 	end
 
 	def update
 		unless @comment.update(comment_params)
 			render :edit and return
 		end
-		redirect_to app_path(@comment.app)
+		redirect_to @comment.commentable
 	end
 
 private
 	def comment_params
-		params.require(:comment).permit(:content, :comment_type, :app_id, :user_id)
+		params.require(:comment).permit(:content, :comment_type)
 	end
 
 	def authenticate
 		unless @comment.user_id == session[:user_id]
 			flash[:alert] = "You don't have authorization to #{@comment.user.name}'s comment!"
-			redirect_to app_path(@comment.app)
+			redirect_to @comment.commentable
 		end
 	end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,12 @@
 class Comment < ActiveRecord::Base
-	belongs_to :app
+	belongs_to :commentable, polymorphic: true
 	belongs_to :user
 
-	validates_presence_of :content, :app_id, :user_id
+	validates_presence_of :content, :commentable, :user_id
 
 	default_scope { order(:created_at => :asc) }
 
-	enum :comment_type => { :contact_status => 0, :app_functionality => 1, :general => 2}
+	def comment_type_name
+		comment_type ? commentable.class.comment_types.keys[comment_type] : nil
+	end
 end
