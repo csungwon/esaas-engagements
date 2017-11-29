262023c129a1e94287225875fdaadc01ed4f2571
Sungwon Cho <csungwon@berkeley.edu>
2017-11-29 00:16:21 -0800

    [feature] Add profile image to each user
    Each user can upload a profile picture from their local disk. In the development
    environment the images are saved in the local disk. In the production environment
    we use Amazon's S3 storage to store images. Paperclip gem was used to implement
    image attributes to users. The default images (for users without profile image)
    were also added to app/assets/images.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20171204012819) do
+||||||| merged common ancestors
+ActiveRecord::Schema.define(version: 20171115224801) do
+=======
+ActiveRecord::Schema.define(version: 20171129080254) do
+>>>>>>> [feature] Add profile image to each user
 
   create_table "apps", force: :cascade do |t|
     t.integer  "org_id"
@@ -99,9 +105,19 @@ ActiveRecord::Schema.define(version: 20171204012819) do
     t.string   "preferred_contact"
     t.string   "sid"
     t.string   "type_user"
+<<<<<<< HEAD
     t.integer  "developing_engagement_id"
     t.integer  "coaching_org_id"
     t.integer  "user_type"
+||||||| merged common ancestors
+    t.integer  "engagement_id"
+=======
+    t.integer  "engagement_id"
+    t.string   "profile_picture_file_name"
+    t.string   "profile_picture_content_type"
+    t.integer  "profile_picture_file_size"
+    t.datetime "profile_picture_updated_at"
+>>>>>>> [feature] Add profile image to each user
   end
 
   add_index "users", ["developing_engagement_id"], name: "index_users_on_developing_engagement_id"
