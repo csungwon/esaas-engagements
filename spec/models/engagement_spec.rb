48c73db7eb2e0590d41a515198cbcfc3de2aea32
Julie Soohoo <jsx1219@berkeley.edu>
2017-11-29 08:38:05 +0000

    start testing file for engagement associations

---
+++ spec/models/engagement_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe Engagement do
+    #Story ID: #153069725
+    describe 'should belong to a coach and contact', :pending => true do
+
+        it 'belongs to a coach', :pending => true do
+            pending("work") do
+                expect(1).to eq(1)
+            end
+        end
+
+        it 'belongs to a client', :pending => true do
+            pending("work") do
+                expect(1).to eq(1)
+            end
+        end
+
+    end
+
+    #Story ID: #153069725
+    describe 'should have some number of developers', :pending => true do
+
+        it "has many developers", :pending => true do
+            pending("work") do
+                expect(1).to eq(1)
+            end
+        end
+
+    end
+
+end
